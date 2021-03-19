6de45e07b0782d0b3bec2a49c19c66a4b214480c
Danny <932755233@qq.com>
2021-03-19 10:53:59 +0800

    添加ignore

---
+++ ExcelUtils.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+import xlrd
+
+workbook = xlrd.open_workbook('/Users/danny/Desktop/common-documents/工作文档/采购入库/入库的编辑界面.xls')  # 打开Excel
+sheet = workbook.sheets()[0]
+rows = sheet.nrows
+
+# for i in range(rows):
+#     print(sheet.row_values(i)[:13])
+
+cols = sheet.ncols
+for i in range(cols):
+    str = sheet.col_values(i)[0]
+    print('String %s;' % str, end='')
+    print('//%s' % sheet.col_values(i)[1])
+    if str[-2:] == 'ID':
+        print('String %sNAME;' % str[:-2], end='')
+        print('//%s名称' % sheet.col_values(i)[1])
+
+print(cols)
