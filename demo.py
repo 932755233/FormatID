8dbd77a21f2a928218a857d7cfe325611715f9ac
Danny <932755233@qq.com>
2021-04-13 00:10:23 +0800

    按键精灵
    python模仿按键精灵搞仓库设置

--- demo.py
+++ demo.py
@@ -1,10 +1,14 @@
 # coding:utf-8
 
 
-name = 1111
-print(name)
-name = input()
-name = int(name)
-print(id(name))
-print(type(name))
-print(name)
+# name = 1111
+# print(name)
+# name = input()
+# name = int(name)
+# print(id(name))
+# print(type(name))
+# print(name)
+
+str = '圣牧库'+'-%s库位'
+index = 1
+print(str%index)
