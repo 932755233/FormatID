22299a18b653ebbb8297208ff8b6e2a9e1c5d9b7
Danny <932755233@qq.com>
2021-04-29 03:02:48 +0800

    更新

--- demo.py
+++ demo.py
@@ -8,7 +8,12 @@
 # print(id(name))
 # print(type(name))
 # print(name)
+#
+# str = '圣牧库'+'-%s库位'
+# index = 1
+# print(str%index)
 
-str = '圣牧库'+'-%s库位'
-index = 1
-print(str%index)
+for index in range(15):
+    print('this.name'+str(index)+'=str['+str(index)+'];')
+    # print('"name'+str(index)+'",',end="")
+# print('String', 'name' + str(index)+";")
