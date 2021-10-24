bf075d0c23698ecb0322900f721d29c4ffe7e2ea
Danny <932755233@qq.com>
2021-10-24 21:13:26 +0800

    更新

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def print_hi(name):
 # open('/Users/danny/Desktop/petshop/centre/src/main/res/layout/layout_purchase_request_detail.xml', 'r') as xml:
 # print(xml) # for str in xml.readline() #     print str finally: if xml: xml.close()
 # path = '/Users/danny/Desktop/petshop/centre/src/main/res/layout/item_sell_out_warehouse.xml'
-# path = 'G:\\work\\petshop\\centre\\src\\main\\res\\layout\\item_wmstask_material.xml'
-path = 'G:\\work\\petshop\\agent\\src\\main\\res\\layout\\item_mission_confirm.xml'
+# path = 'G:\\work\\petshop\\centre\\src\\main\\res\\layout\\layout_customer_cancellation.xml'
+path = 'G:\\work\\petshop\\agent\\src\\main\\res\\layout\\item_market_theout.xml'
 tempStr = ''
 # with open(path, 'rb') as lines:
 #     for line in lines.readline():
@@ -41,11 +41,21 @@ for line in fileinput.input(path, openhook=fileinput.hook_encoded('utf-8')):
     tempStr = line
 
 print()
+tempStrSS = ''
 for line in fileinput.input(path, openhook=fileinput.hook_encoded('utf-8')):
+
     if line.find('android:id="@+id/') >= 0:
         temp = line.split('/')[1][:-2]
+        if temp[-3:] == 'red':
+            continue
+        if temp[-3:] == 'txt':
+            tempStrSS = ''
+            continue
         print('@ViewById(R.id.%s)' % temp)
-        print('TextView %s;' % temp)
+        print('TextView %s;//%s' % (temp, tempStrSS))
+    if line.find('android:text=') >= 0:
+        if tempStrSS == '':
+            tempStrSS = line.split('=')[1][1:-6]
     tempStr = line
 
 # 自动生成注释和控件
