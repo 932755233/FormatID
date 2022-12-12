26e8b5e9f91b937391f305612fada28e9eb6e0ea
Danny <932755233@qq.com>
2022-12-12 23:19:13 +0800

    在javbus上获取图片

--- 根据番号查图片/queryPicAndInfoForAVCodeOfJavBus.py
+++ 根据番号查图片/queryPicAndInfoForAVCodeOfJavBus.py
@@ -15,7 +15,7 @@ headers = {
     'accept-language': 'zh-CN,zh;q=0.9'
 }
 
-javBus = r'https://www.javbus.com/'
+javBus = r'https://www.javbus.com'
 
 finderPath = r'Z:\LSP\Adult Video'
 
@@ -24,72 +24,72 @@ file_name_list = os.listdir(finderPath)
 avCodeList = []
 
 
-def walkFile():
+def walkFilePath():
     for fileStr in file_name_list:
-        if (fileStr[-3:] == 'jpg') | (fileStr[-3:] == 'JPG'):
-            continue
-        if fileStr[-2:] == 'db':
-            continue
-        if (fileStr[-3:] == 'mp4') | (fileStr[-3:] == 'MP4'):
-            fileStr = fileStr[:-4]
-        if (fileStr[-3:] == 'avi') | (fileStr[-3:] == 'AVI'):
-            fileStr = fileStr[:-4]
-        avcode = fileStr
-        if avcode[-1] == 'C':
-            avcode = avcode[:-1]
-        if avcode[-1] == '-':
-            avcode = avcode[:-1]
-        if avcode.find(' ') != -1:
-            avcode = avcode[:avcode.find(' ')]
-
-        fileD = {'filename': fileStr, 'avcode': avcode}
-
-        avCodeList.append(fileD)
+        # print(fileStr)
+        tempPath = os.path.join(finderPath, fileStr);
+        # print(os.path.isdir(tempPath))
+        if os.path.isdir(tempPath):
+            for tempFileName in os.listdir(tempPath):
+                file = os.path.splitext(tempFileName)
+                print(file[1])
+                print(tempFileName)
+            fileBean = {'path': tempPath, 'avcode': fileStr}
+            avCodeList.append(fileBean)
 
 
-def network(avCode):
-    url = javBus + avCode
+def network(fileBean):
+    url = javBus + '/' + fileBean['avcode']
     print(url)
     response = requests.get(url, headers=headers, proxies=proxies)
     urlText = response.text
-    with open('./yellow.html', 'w', encoding='utf-8') as fp:
+    with open('./javbus_yellow.html', 'w', encoding='utf-8') as fp:
         fp.write(urlText)
     soup = BeautifulSoup(urlText, 'html.parser')
     a = soup.find('a', attrs={'class': 'bigImage'})
     print(javBus + a.img['src'])
-    print(a.img['title'])
+    print(a.img['title'])  # 标题
     if a is None:
         return 'F'
     else:
+        fileBean['piUrl'] = javBus + a.img['src']
         return javBus + a.img['src']
 
 
-def savePicture(picUrl, picName):
-    imgData = requests.get(picUrl, headers=headers).content
-    picPath = finderPath + '\\' + picName + '-poster.jpg'
+def savePicture(fileBean):
+    imgData = requests.get(fileBean['piUrl'], headers=headers, proxies=proxies).content
+    # picPath = path + os.path.sep + 'folder.jpg'
+    picPath = './pic/' + fileBean['avcode'] + '.jpg'
     with open(picPath, 'wb') as fp:
         fp.write(imgData)
     return picPath
 
 
+def saveNfo(fileBean):
+    picPath = './pic/'+ '111.nfo'
+    nfoText = '你好'
+    with open(picPath, 'w',encoding='utf-8') as fp:
+        fp.write(nfoText)
+
+
 def doTask():
-    walkFile()
-    print('文件有：', len(avCodeList))
-    for fileStr in avCodeList:
-        print('------------------开始：' + fileStr['filename'])
-        picUrl = network(fileStr['avcode'])
-        if 'F' in picUrl:
-            fileStr['result'] = '未找到'
-            print('------------------------未找到')
-        else:
-            fileStr['result'] = savePicture(picUrl, fileStr['filename'])
-        print('------------------结束：' + fileStr['filename'])
-    print('------------------完成------------------')
+    walkFilePath()
+    # for fileBean in avCodeList:
+    fileBean = avCodeList[0]
+    resultNet = network(fileBean)
+    if 'F' in resultNet:
+        fileBean['result'] = '未找到'
+    else:
+        fileBean['picPath'] = savePicture(fileBean)
+        saveNfo(fileBean)
+    print(fileBean)
 
 
 if __name__ == '__main__':
+    saveNfo('')
     # doTask()
+    # walkFilePath()
+    # network('STCV-202')
     # walkFile()
-    network('STCV-202')
-    for fileStr in avCodeList:
-        print(fileStr)
+    # for fileStr in avCodeList:
+    #     print(fileStr)
